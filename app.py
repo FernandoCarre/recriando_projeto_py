73f0d0719cb8cd015b136f3d31af68fa5f8ce1d6
Fernando Carré <contatofernandocarre@gmail.com>
2025-07-08 11:03:09 -0300

    c03: finalizado: ultimas correções e codigos adicionados

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ restaurantes = [{'nome':'Fernando Sushi', 'categoria':'Japonesa', 'ativo': False
 #porque eu não fechei as chaves de cada dicionário a lista considerou apenas o último dicionário.
 
 def exibir_nome_do_programa():
+    '''Função responsável por mostrar o nome do programa'''
     print('''
 ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 ─██████████████─██████████████─████████████████───████████████████───██████████████────██████████████─████████──████████─██████████████─████████████████───██████████████─██████████████─██████████████─
@@ -20,14 +21,19 @@ def exibir_nome_do_programa():
 ─██████████████─██████──██████─██████──██████████─██████──██████████─██████████████────██████████████─████████──████████─██████─────────██████──██████████─██████████████─██████████████─██████████████─
 ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────''')
 def exibir_opcoes():
+    '''Função responsável por mostrar as opções disponíveis'''
     print('\n1- Cadastrar restaurante\n'
       '2- Listar restaurante\n'
       '3- Alternar restaurante\n'
       '4- Encerrar aplicativo\n')
     
 def exibir_subtitulo(texto):
+    '''Função responsável por mostrar o título de cada opção escolhida'''
+    linha = '*' * len(texto)
     os.system('cls')
+    print(linha)
     print(texto)
+    print(linha)
     print()
     
 def retornar_menu_principal():
@@ -35,6 +41,7 @@ def retornar_menu_principal():
      main()
     
 def cadastro_restaurante():
+    '''Função responsável por cadastrar os restaurantes'''
     exibir_subtitulo('Cadastro de restaurante:')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
@@ -46,18 +53,23 @@ def cadastro_restaurante():
     retornar_menu_principal()
 
 def exibir_restaurantes():
+    '''Função responsável por listar os restaurantes'''
     exibir_subtitulo('Lista de restaurantes cadastrados:')
+    print(f'{'Nome'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
+    #Por algum motivo não estava funcionando, fui conferir com o original e estava igual...
+    #O motivo do erro é porque eu não encerrei o programa e fiquei "runando" ele achando que isso daria reboot.
+    
     for restaurante in restaurantes:
-        ###-meu erro corrigo
+        
         nome_do_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
         ativo = restaurante['ativo']
-        print(f'- {nome_do_restaurante} | {categoria} | {ativo}')
-        #print(f'{restaurante['nome'], restaurante['categoria'], restaurante['ativo']}')
-        ###
+        print(f'- {nome_do_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+       
     retornar_menu_principal()
 
 def alternar_estado_restaurante():
+    '''Função responsável por mudar o estado do restaurante'''
     exibir_subtitulo('Alternar o estado do restaurante:')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
     restaurante_encontrado = False
@@ -75,15 +87,19 @@ def alternar_estado_restaurante():
     retornar_menu_principal()
           
 def finalizar_app():
+    '''Função responsável por encerrar o aplicativo'''
     os.system('cls')
     print('Aplicativo encerrado.')    
 
 def opcao_invalida():
+    '''Função responsável por mostrar uma mensagem de erro'''
     os.system('cls')
     input('Opção inválida! Aperte enter para retornar ao menu principal ')
+    main()
 #a def precisa vir antes de ser chamada, caso contrário não funcionária no código
 
 def menu_escolhas():
+    '''Função responsável por direcionar o usuário para a opção escolhida'''
     try:
         escolha_opcao = int(input('Escolha uma opção: '))
         #eu botei ele fora e o except não funcionava
@@ -102,6 +118,7 @@ def menu_escolhas():
         opcao_invalida()
 
 def main():
+    '''Função responsável por iniciar as funções e tornar a manutenção do código mais fácil'''
     os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
